16da1f82832da57adb2300c5ac7629a057bccb3f
Naveen <naveenkumar2703@gmail.com>
2016-01-28 19:59:41 -0500

    Breadth first and depth first
    Initial version of code

--- PA1/src/DistanceFinder.py
+++ PA1/src/DistanceFinder.py
@@ -9,13 +9,20 @@ class DistanceFinder (object):
         self.distance_directory = distance_directory
         self.origin = origin
         self.destination = destination
+        self.route = None
 
     #leaving as abstract - to be overridden by child classes
     def searchRoute(self):
         return
 
-    def addToDistance(distance,totalDistanceSofar):
-        return totalDistanceSofar + distance.distance()
+    def distance_between_cities(self,city1,city2):
+        if city1 == city2:
+            return 0
+        for dist in self.distance_directory.get(city1):
+            if dist.city1 == city2 or dist.city2 == city2:
+                return dist.distance
+                
+        return 0
         
     def removeFromDistance(distance,totalDistanceSofar):
         return totalDistanceSofar - distance.distance()
@@ -23,13 +30,44 @@ class DistanceFinder (object):
     def checkIsDestination(self,currentLocation):
         return self.destination == currentLocation
 
-    def printRoute(self):
-        route = self.origin
+    def printRoute(self,destination):
+        route = ''
+        distance = 0
+        from_city = None
+        to_city = None
 
-        for city in in_btw_cities:
-            route += ', ' + city
+        if self.origin == self.destination:
+            self.route = [self.origin,self.destination]
+        else:
+            self.route = [destination.city]
+            parent_city = destination.prev_city
+                
+            while parent_city.prev_city != None:
+                self.route.append(parent_city.city)
+                parent_city = parent_city.prev_city
+            else:
+                self.route.append(parent_city.city)                    
 
-        route += ', ' + self.destination
+            self.route = list(reversed(self.route))
+            print self.route
+                
+
+        for city in self.route:
+            route += city + ', '
+            if from_city == None:
+                from_city = city
+            elif to_city == None:
+                to_city = city
+                distance += int(self.distance_between_cities(from_city,to_city))
+                from_city = to_city
+                to_city = None
+            #else:
+                #call calculate dist bwn cities function and swap cities
+
+        route += ' - ' + str(distance)
         print route
+
+    def is_dead_end(self,location):
+        return not len(location.next_location_names) > 0
         
         
--- PA1/src/location.py
+++ PA1/src/location.py
@@ -1,10 +1,26 @@
 class location(object):
     def __init__(self,city,prev_city,route_dict):
         self.city = city
-        self.prev_city = prev_city
-        self.route_dict = route_dict
-        self.next_city = construct_path(city,prev_city,route_dict)
-    def construct_path(city,prev_city,route_dict):
-        nxt_city_lst = route_dict.get(city, default = "none")
-        nxt_city_lst.remove(prev_city)
+        self.prev_city = prev_city#this will serve as parent node
+        self.next_location_names = get_next_location_names(city,prev_city,route_dict)
+        self.next_locations = None
+        self.state = 'none'
+        
+def get_next_location_names(currentcity,prev_city,route_dict):
+        nxt_city_lst = []
+        
+        for dist in route_dict.get(currentcity):
+            #print dist
+            if prev_city != None and (dist.city1 != prev_city.city or dist.city2 != prev_city.city):
+                nxt_city_lst.append(get_other_city(dist, currentcity))               
+            elif prev_city == None:
+                nxt_city_lst.append(get_other_city(dist, currentcity))
         return nxt_city_lst
+
+
+
+def get_other_city(distance,currentcity):
+    if distance.city1 == currentcity:
+        return distance.city2
+    elif distance.city2 == currentcity:
+        return distance.city1
